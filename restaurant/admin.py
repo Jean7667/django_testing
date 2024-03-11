39d326ac1a1cafa3da1b09f25b279528d9e48f67
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-03-11 19:59:38 +0000

    added each class to admin.py superuser created  - all tested - OK

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from restaurant.models import Restaurant, Sale, Rating
 
 # Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Sale)
+admin.site.register(Rating)
