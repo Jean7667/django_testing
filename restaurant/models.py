f0b96260ae12eae598d4d8f2c7c8fabbe8133738
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-03-11 19:29:25 +0000

    rating model created and added to the db

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+#import default user model
+from django.contrib.auth.models import User 
 # Create your models here.
 
 # Restaurant 
@@ -22,3 +23,11 @@ class Restaurant(models.Model):
 
 
 # Rating    
+    
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"Rating: {self.rating}"
