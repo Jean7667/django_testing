06328e05ed8bf6388d82e51d1ae3d28999d2f8d3
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-03-12 21:37:23 +0000

    new model for User_Profile

--- restaurant/models.py
+++ restaurant/models.py
@@ -48,4 +48,29 @@ class Sale(models.Model):
     #change in case of deletion ie. the value will be empty null=True
     income = models.DecimalField(max_digits=8, decimal_places=2)
     datetime = models.DateTimeField()
+
+
+#signals practice check test sync with user 
+class Type_of_Business (models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Type of business: {self.name}"
+
+class User_Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    first_name = models.CharField(max_length=100, blank=False)
+    last_name = models.CharField(max_length=150, blank=False)
+    username = models.CharField(max_length=150, unique=False, blank=False)
+    email = models.EmailField(max_length=150, blank=False)
+    location = models.CharField(max_length=150, blank=False)
+    website = models.URLField(max_length=200, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    company_name = models.CharField(max_length=200, blank=True)
+    is_a_business = models.BooleanField(default=False)
+    business = models.ForeignKey(Type_of_Business, on_delete=models.SET_NULL, null=True, blank=True)
     
+    def __str__(self):
+        return f"username: {self.username}"
